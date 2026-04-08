354c10f7b3fe23f176b03c11997b5a25faa92069
riyaskt25 <riyaskt25@gmail.com>
2026-04-08 17:54:18 +0530

    initial commit: Project files created using spring initializer

---
+++ src/main/java/com/snb/ms/MsTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.snb.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTemplateApplication.class, args);
+	}
+
+}
